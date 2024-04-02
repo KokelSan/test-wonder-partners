5e6cbab75f6434a36eb7f0587af48b8d851fbf35
Theo Kokel <[theokok@hotmail.fr]>
2024-04-02 18:35:32 +0200

    Added interface and base interactions

---
+++ Assets/Scripts/Managers/ShowroomManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowroomManager : MonoBehaviour
+{
+    public List<ShowroomModel> Models;
+    public ShowroomNavigationButtonsSO ButtonsDefinition;
+    public HorizontalLayoutGroup ButtonsParent;
+    public ShowroomNavigationButton NavigationButtonPrefab;
+
+    private int _currentModelIndex = 0;
+    private List<ShowroomNavigationButton> _instantiatedButtons = new List<ShowroomNavigationButton>();
+    private int _currentActiveButtonIndex = 0;
+
+    private void Start()
+    {
+        LoadModel();
+    }
+
+    private void LoadModel()
+    {
+        ShowroomModel model = Models[_currentModelIndex];
+        CreateButtonsForModel(model);
+    }
+
+    private void CreateButtonsForModel(ShowroomModel model)
+    {
+        foreach (ShowroomNavigationTransform navTransform in model.NavigationTransforms)
+        {
+            NavigationButton navButton = ButtonsDefinition.Buttons.Find(button => button.Label == navTransform.Label);
+            if (navButton != null)
+            {
+                ShowroomNavigationButton showroomButton = Instantiate(NavigationButtonPrefab, ButtonsParent.transform);
+                showroomButton.SetButton(navButton.Label, navButton.ActiveIcon, navButton.InactiveIcon, navTransform.IsStartingTransform);
+                _instantiatedButtons.Add(showroomButton);
+
+                int index = _instantiatedButtons.Count - 1;
+                showroomButton.ActivationButton.onClick.AddListener(() => OnButtonClicked(index));
+                if (navTransform.IsStartingTransform) _currentActiveButtonIndex = index;
+                
+                continue;
+            }
+            Debug.LogWarning($"Button with label '{navTransform.Label}' required by model '{model.name}' is not defined");
+        }
+    }    
+
+    private void OnButtonClicked(int buttonIndex)
+    {
+        ShowroomNavigationButton button = _instantiatedButtons[buttonIndex];
+        NavigationLabel label = button.Label;
+        _instantiatedButtons[_currentActiveButtonIndex].SetActive(false);
+        button.SetActive(true);
+        _currentActiveButtonIndex = buttonIndex;
+        Models[_currentModelIndex].PerformTransition(label);
+    }    
+}
---
+++ Assets/Scripts/Showroom/ShowroomNavigationButton.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Animator))]
+public class ShowroomNavigationButton : MonoBehaviour
+{
+    private const string ShowAnimatorTriggerName = "Show";
+    private const string HideAnimatorTriggerName = "Hide";
+
+    public Button ActivationButton;
+    [SerializeField] private GameObject ActiveElements;
+    
+    [SerializeField] private TMP_Text DisplayNameText;
+    [SerializeField] private Image ActiveImage;
+    [SerializeField] private Image InactiveImage;
+
+    public NavigationLabel Label => _label;
+    private NavigationLabel _label;
+    private Animator _animator;
+
+    private void Start()
+    {
+        if (!TryGetComponent(out _animator))
+        {
+            Debug.LogError($"{name} has no animator");
+        }
+    }
+
+    public void SetButton(NavigationLabel navLabel, Sprite activeImage, Sprite inactiveImage, bool isActive)
+    {
+        name = $"{navLabel}_Button";
+        _label = navLabel;
+        DisplayNameText.text = navLabel.ToString();
+        ActiveImage.sprite = activeImage;
+        InactiveImage.sprite = inactiveImage;
+        SetActive(isActive);
+    }
+
+    public void SetActive(bool isActive)
+    {
+        ActiveElements.SetActive(isActive);
+        ActivationButton.interactable = !isActive;
+    }
+}
